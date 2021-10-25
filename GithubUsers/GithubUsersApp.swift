d4dcd4e7f3b539ffac37221d2a79bb4a2c173522
kingpogi123 <43062730+kingpogi123@users.noreply.github.com>
2021-10-25 23:07:38 +0800

    Initial Commit

---
+++ GithubUsers/GithubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsersApp.swift
+//  GithubUsers
+//
+//  Created by King Copones on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
